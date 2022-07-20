072e47da5ff0929bf9f6ce59472cf114e7239382
aggeev <den.ageev.2014@mail.ru>
2022-07-20 15:03:32 +0300

    домашнее задание № 10

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int number,number2,number3;
+number = new Random().Next(100,999);
+Console.WriteLine(" число " + number);
+number2 = (number / 10) %10;
+number3 = (number + 10) %10;
+    Console.WriteLine(" второе ичисло " + number2);
+    Console.WriteLine(" третье число " + number3);
