79fe058f9d7b2fc478c2f8469dcd2dff51994213
aggeev <den.ageev.2014@mail.ru>
2022-07-20 17:46:20 +0300

    Д З

--- Program.cs
+++ Program.cs
@@ -1,40 +1,13 @@
-﻿
-int number,number2,number3;
-number = new Random().Next(100,999);
-Console.WriteLine(" число " + number);
-number2 = (number / 10) %10;
-number3 = (number + 10) %10;
-    Console.WriteLine(" второе число " + number2);
-    Console.WriteLine(" третье число " + number3);
-//......
+﻿ // Задача № 10;
 
-Console.WriteLine("введите день недели");
-int input = Convert.ToInt32(Console.ReadLine());
+
+ int number,number2,number3;
+
+ number = new Random().Next(100,999);
+ Console.WriteLine(" число "+ number);
+ number2 =(number / 10) % 10;
+ number3 =(number + 10) % 10;
+ Console.WriteLine(" второе число " + number2);
+ Console.WriteLine(" третье число "+ number3);
+
  
- switch (input)
- {
-    case 1:
-        Console.WriteLine("понедельник");
-        break;
-    case 2:
-        Console.WriteLine("вторник");
-        break;
-    case 3:
-        Console.WriteLine("среда");
-        break;
-    case 4:
-        Console.WriteLine("четверг");
-        break;
-    case 5:
-        Console.WriteLine("пятница");
-        break;
-    case 6:
-        Console.WriteLine("суббота");
-        break;
-    case 7:
-        Console.WriteLine("воскресенье"); 
-        break;                      
-    default:
-        Console.WriteLine("такого дня не существует ,ошибка");
-        break;
- }
