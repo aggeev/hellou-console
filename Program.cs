3ffff463cba40f22e544e783a69c3a878d8502c0
aggeev <den.ageev.2014@mail.ru>
2022-07-20 18:34:00 +0300

    Hello Word

--- Program.cs
+++ Program.cs
@@ -1,13 +1,1 @@
-﻿ // Задача № 10;
-
-
- int number,number2,number3;
-
- number = new Random().Next(100,999);
- Console.WriteLine(" число "+ number);
- number2 =(number / 10) % 10;
- number3 =(number + 10) % 10;
- Console.WriteLine(" второе число " + number2);
- Console.WriteLine(" третье число "+ number3);
-
- 
+﻿Console.WriteLine("Hello Word");
